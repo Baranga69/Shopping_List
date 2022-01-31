838d2d53d4e69a865e442746abdaa6d94aa6b6d2
Baranga69 <69077312+Baranga69@users.noreply.github.com>
2022-01-31 14:48:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shopping_List
+ This is another attempt at a CRUD application in Visual Studio 2022 with C# as a Windows Form Application
---
+++ Shopping_List/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shopping_List
+{
+    public partial class Form1 : Form
+    {
+        private const string ConnectionString = "Data Source=B1T3M3\\SQLEXPRESS;Initial Catalog=Personal_Shopping_List;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(ConnectionString);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into shopping_List values ('"+(textBox1.Text)+"', '"+int.Parse(textBox2.Text)+"', '"+int.Parse(textBox3.Text)+"', getdate())", conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully Inserted");
+            conn.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox1.Focus();
+            BindData();
+        }
+
+        void BindData()
+        {
+            SqlCommand cmd = new SqlCommand("select * from shopping_List", conn);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("update shopping_List set ItemName = '" + textBox1.Text + "', ItemQuantity = '" + textBox2.Text + "', ItemPrice = '" + textBox3.Text + "' where ItemID = '" + int.Parse(textBox4.Text) + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Successfully Updated");
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                if (MessageBox.Show("Are you sure you want to delete?", "Delete Record", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("Delete shopping_List where ItemID= '" + int.Parse(textBox4.Text) + "'", conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Successfully deleted!");
+                    BindData();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Input Item ID");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("select * from shopping_List where ItemID = '" + int.Parse(textBox4.Text) + "'", conn);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
